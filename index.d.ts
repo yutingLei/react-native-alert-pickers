1afe2634d2a9487751fa85fa22f4a01db9c75064
yutingLei <lyt_930617@163.com>
2018-03-12 17:15:58 +0800

    Changed: interface

--- index.d.ts
+++ index.d.ts
@@ -1,102 +1,77 @@
-import React from "react";
-import { ViewProperties } from "react-native";
+declare module "react-native-alert-pickers" {
+  interface ColorPickerTypes {
+    /**
+     * 使用16进制显示颜色(默认true)
+     */
+    useHex?: true | false;
 
-export interface ColorPickerTypes extends ViewProperties {
-  /**
-   * 使用16进制显示颜色
-   *
-   * @type {boolean}
-   * @memberOf ColorPickerTypes
-   * @default true
-   */
-  useHex?: boolean;
+    /**
+     * 是否显示颜色选择器(true | false)
+     *
+     * @requires
+     */
+    visible: true | false;
 
-  /**
-   * 是否显示颜色选择器
-   *
-   * @type {boolean}
-   * @memberOf ColorPickerTypes
-   */
-  visible: boolean;
+    /**
+     * 颜色选中触发函数
+     */
+    onPicked?: (title: string) => void;
 
-  /**
-   * 颜色选中触发函数
-   *
-   * @type {func}
-   * @memberOf ColorPickerTypes
-   * @default undefined
-   */
-  onPicked?: (title: string) => void;
+    /**
+     * 选中按钮标题(默认"Done")
+     */
+    selectTitle?: "Done";
+  }
 
-  /**
-   * 选中按钮标题
-   *
-   * @type {string}
-   * @memberOf ColorPickerTypes
-   * @default Done
-   */
-  selectTitle?: string;
+  interface ButtonOptions {
+    /**
+     * 按钮标题
+     */
+    title: string;
+    /**
+     * 按钮类型("default"|"cancel"|"destructive")
+     */
+    style?: "default" | "cancel" | "destructive";
+    /**
+     * 按钮标题颜色(e.g. "deepskyblue")
+     */
+    color?: "deepskyblue";
+  }
+
+  interface SimpleAlertTypes {
+    /**
+     * 是否显示颜色选择器(true|false)
+     */
+    visible: true | false;
+
+    /**
+     * 提示标题
+     */
+    title?: string;
+    /**
+     * 提示信息
+     */
+    message?: string;
+    /**
+     * 提示按钮(alert is [{ title: "Done", style: "cancel"}] or
+     *         actionSheet is [{ title: "Done", color: "deepskyblue"}]
+     *        )
+     */
+    buttons?: [ButtonOptions];
+    /**
+     * 取消按钮在buttons中的下标(仅限actionSheet模式)
+     */
+    cancelIndex?: 0;
+    /**
+     * 提示模式("alert"|"actionSheet")
+     */
+    alertType?: "alert";
+    /**
+     * 点击按钮后触发
+     */
+    onTouched?: (title: string) => void;
+  }
+
+  export class ColorPicker extends React.Component<ColorPickerTypes> {}
+  export class SimpleAlert extends React.PureComponent<SimpleAlertTypes> {}
 }
-
-export interface SimpleAlertTypes extends ViewProperties {
-  /**
-   * 是否显示颜色选择器
-   *
-   * @type {boolean}
-   * @memberOf SimpleAlertTypes
-   */
-  visible: boolean;
-
-  /**
-   * 提示标题
-   *
-   * @type {string}
-   * @memberOf SimpleAlertTypes
-   * @default null
-   */
-  title?: string;
-  /**
-   * 提示信息
-   *
-   * @type {string}
-   * @memberOf SimpleAlertTypes
-   * @default null
-   */
-  message?: string;
-  /**
-   * 提示按钮
-   *
-   * @type {arrayOf(Object)}
-   * @memberOf SimpleAlertTypes
-   * @default alert: [{ title: "Done", style: "cancel" }]
-   *          actionSheet: [{ title: "Done", color: "deepskyblue" }]
-   */
-  buttons?: [object];
-  /**
-   * 取消按钮在buttons中的下标(仅限actionSheet模式)
-   *
-   * @type {number}
-   * @memberOf SimpleAlertTypes
-   * @default 0
-   */
-  cancelIndex?: number;
-  /**
-   * 提示模式
-   *
-   * @type {string}
-   * @memberOf SimpleAlertTypes
-   * @default alert
-   */
-  alertType?: string;
-  /**
-   * 是否显示颜色选择器
-   *
-   * @type {func}
-   * @memberOf SimpleAlertTypes
-   * @default null
-   */
-  onTouched?: (title: string) => void;
-}
-
-export class ColorPicker extends React.Component<ColorPickerTypes> {}
-export class SimpleAlert extends React.PureComponent<SimpleAlertTypes> {}
